343b2f2bfe6cb87d1197c81236bd9bfe47bd6375
Divakar chouhan <divakarchouhan251@gmail.com>
2022-11-18 10:35:14 +0530

    add skills

---
+++ index.js
@@ -0,0 +1,4 @@
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
