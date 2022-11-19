03815c310c92f2f79f4e1d4ecbc19832e30e2614
Divakar chouhan <divakarchouhan251@gmail.com>
2022-11-19 11:09:49 +0530

    skill section complete

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const slidesContainer = document.getElementById("slides-container");
 const slide = document.querySelector(".slide");
-const prevButton = document.getElementById("slide-arrow-prev");
+
 const nextButton = document.getElementById("slide-arrow-next");
+const prevButton = document.getElementById("slide-arrow-prev");
+
+nextButton.addEventListener("click", () => {
+    const slideWidth=slide.clientWidth;
+    slidesContainer.scrollLeft+=slideWidth;
+});
+prevButton.addEventListener("click", () => {
+    const slideWidth=slide.clientWidth;
+    slidesContainer.scrollLeft-=slideWidth;
+});
